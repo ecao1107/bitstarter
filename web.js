2a307ae2a6a05aab52cfea7486b2cda7d56d4cec
ecao1107@gmail.com <ubuntu@ip-172-31-47-145.us-west-2.compute.internal>
2013-07-10 23:20:00 +0000

    Modified web.js to read Index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var content = fs.readFileSync( 'index.html' );
+  var content = fs.readFileSync( 'index.html', 'utf-8' );
   buf = new Buffer(256);
   buf.write( content );
   response.send(buf.toString());
